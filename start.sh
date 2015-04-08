c1ef3cbb94dc8dc072bfa3e59996611c4d0efbcd
tanaka0323 <tanaka@infocorpus.com>
2015-04-08 11:51:41 +0900

    Modify exec command.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -e
 sed -i "s|^elasticsearch_url:.*|elasticsearch_url: \"http://$ELASTICSEARCH_PORT_9200_TCP_ADDR:$ELASTICSEARCH_PORT_9200_TCP_PORT\"|" /opt/kibana/config/kibana.yml
 
 # Executing
-/opt/kibana/bin/kibana
+exec /opt/kibana/bin/kibana
