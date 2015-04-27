cd7b8b7c938bec8b313543b3e8f174113464ce75
tanaka0323 <tanaka@infocorpus.com>
2015-04-28 02:00:42 +0900

    Update elastic search alias name.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = "/opt/kibana/bin/kibana" ]; then
-    sed -i "s|^elasticsearch_url:.*|elasticsearch_url: \"http://elasticsearch:9200\"|" /opt/kibana/config/kibana.yml
+    sed -i "s|^elasticsearch_url:.*|elasticsearch_url: \"http://$ELASTICSEARCH_PORT_9200_TCP_ADDR:9200\"|" /opt/kibana/config/kibana.yml
 fi
 
 exec "$@"
